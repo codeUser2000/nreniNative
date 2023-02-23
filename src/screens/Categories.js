d11e3ec28cda56ca14217c5d49298a9fc13859b9
unmatched0 <hovhannisya03@mail.ru>
2023-02-23 16:51:37 +0400

    Home.js

---
+++ src/screens/Categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from "react-native";
+
+function Categories(props) {
+    return (
+        <View>
+
+        </View>
+    );
+}
+
+export default Categories;
